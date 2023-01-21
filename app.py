439a445569155bbdb653861abe239c3c3edb2eaf
Mingchung <65872029+mingchungx@users.noreply.github.com>
2023-01-21 15:11:15 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -16,26 +16,29 @@ def cohere_run_m(category):
     # print(category)
     return jsonify(generateTasks(category))
 
+# This might need to change, i'm not really sure how uploading BLOB works or what data is being recieved
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    file_data = file.read()
-    s3.put_object(Bucket=bucket_name, Key=file.filename, Body=file_data)
+    with open(file, 'rb') as file_data:
+        s3.put_object(Bucket=bucket_name, Key=file.filename, Body=file_data)
     return jsonify({'message': 'File uploaded successfully'})
 
-# download (filename) takes the filename in the AWS S3 bucket and returns the universal URL to view it
-@app.route('/download/<string:filename>', methods=['GET'])
-def download(filename):
-    data = s3.get_object(Bucket=bucket_name, Key=filename)
-    file_data = data['Body'].read()
-    print(file_data)
+# download (filename) takes the user with meta data in the dynamodb and in the AWS S3 bucket and returns the universal URL to view it and caption
+@app.route('/download/<string:user>', methods=['GET'])
+def download(user):
+    imageData = retrieveImage(user)
+    caption = imageData[0]
+    filename = imageData[1]
 
-    url = s3.generate_presigned_url('get_object', Params={
+    data = s3.get_object(Bucket=bucket_name, Key = filename)
+    url = s3.generate_presigned_url('get_object', Params = {
         'Bucket': bucket_name,
         'Key': filename
     }, ExpiresIn = 3600)
 
-    return url
+    return jsonify([url, caption])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
